221a7faacd9306ae35da6df957547303006a512f
Runescapechar <timoteochar01@gmail.com>
2020-06-04 17:20:57 +0000

    Macro

--- Downloads.bat
+++
@@ -1,4 +0,0 @@
-start chrome.exe https://download.teamviewer.com/download/TeamViewer_Setup.exe
-start chrome.exe https://remotedesktop.google.com/
-start chrome.exe https://central.github.com/deployments/desktop/desktop/latest/win32
-start chrome.exe https://content.runescape.com/downloads/windows/RuneScape-Setup.exe?crc=448032095#_ga=2.207639250.1595187355.1591288157-159139054.1591288157
